3d5a1371ed023ae7ddce5db17769b59eec96222c
zahiq-ibrahim <zahiq.ibrahim@enablenow.com>
2026-09-01 20:22:16 +0530

    added basic view movie page

--- MOVIES/movie_page.js
+++ MOVIES/movie_page.js
@@ -1,6 +1,17 @@
 const title = document.querySelector("title");
-const container = document.querySelector("#movie-container");
+//const container = document.querySelector("#movie-container");
 const movieTitle = document.querySelector("#movie-title");
+const tagline = document.getElementById("tagline");
+const description = document.getElementById("description");
+const rating = document.getElementById("rating-value");
+const releaseDate = document.getElementById("release-date");
+const duration = document.getElementById("duration");
+const language = document.getElementById("original-language");
+const country = document.getElementById("origin-country");
+const movieStatus = document.getElementById("status");
+const genres = document.querySelector(".genre-bubble-row");
+const playBtn = document.getElementById("play-now-btn");
+const cardsDiv = document.querySelector(".production-com");
 
 const movieId = "969681";
 //calling
@@ -15,6 +26,49 @@ async function loadMovie() {
 
   title.textContent = movie.data.title;
   movieTitle.textContent = movie.data.title;
+  tagline.textContent = movie.data.tagline;
+  description.textContent = movie.data.overview;
+  rating.textContent = movie.data.vote_average;
+  releaseDate.textContent += movie.data.release_date;
+  duration.textContent += `${movie.data.runtime} min`;
+  language.textContent += movie.data.original_language.toUpperCase();
+  country.textContent = movie.data.origin_country;
+  movieStatus.textContent = movie.data.status;
+
+  movie.data.genres.forEach((genre) => {
+    const newGenreDiv = document.createElement("div");
+    const newGenreSpan = document.createElement("span");
+    newGenreSpan.textContent = genre.name;
+    newGenreDiv.classList.add("genre-bubble");
+    newGenreDiv.append(newGenreSpan);
+
+    genres.append(newGenreDiv);
+  });
+  playBtn.addEventListener('click', () =>{
+
+    window.open(movie.data.stream_url, "_blank");
+    
+  });
+
+  movie.data.production_companies.forEach((company)=>{
+    const newCard = document.createElement("div");
+    newCard.classList.add("prod-info-card");
+    
+    const image = document.createElement("img");
+    image.src = `https://image.tmdb.org/t/p/w500/${company.logo_path}`;
+    image.alt = "img";
+    const spanName = document.createElement('span');
+    spanName.textContent = company.name;
+    const spanCu = document.createElement('span');
+    spanCu.textContent =company.origin_country;
+
+    newCard.append(image);
+    newCard.append(spanName);
+    newCard.append(spanCu);
+    cardsDiv.append(newCard);
+
+  });
+  
 }
 
 async function getMovie(movieId) {
